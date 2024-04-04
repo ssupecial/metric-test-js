05597c5e7c63a01404750b4f69ba4ec664deebe3
Kang-Eunsu <kanges802@g.skku.edu>
2024-04-04 13:27:10 +0900

    modify get instance id

--- src/metric.ts
+++ src/metric.ts
@@ -7,74 +7,90 @@ import {
   PeriodicExportingMetricReader
 } from '@opentelemetry/sdk-metrics'
 import * as http from 'http';
-import { EC2MetadataCredentials } from 'aws-sdk';
+import { EC2Client, GetInstanceMetadataDefaultsCommand } from "@aws-sdk/client-ec2";
 
 
-const getInstanceId = (): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    const options = {
-      hostname: '169.254.169.254',
-      path: '/latest/meta-data/instance-id',
-      method: 'GET',
-      timeout: 1000,
-    };
+// const getInstanceId = (): Promise<string> => {
+//   return new Promise((resolve, reject) => {
+//     const options = {
+//       hostname: '169.254.169.254',
+//       path: '/latest/meta-data/instance-id',
+//       method: 'GET',
+//       timeout: 1000,
+//     };
 
-    const req = http.request(options, (res) => {
-      let data: string = '';
-      res.on('data', (chunk: Buffer) => {
-        data += chunk.toString();
-      });
-      res.on('end', () => {
-        resolve(data);
-      });
-    });
+//     const req = http.request(options, (res) => {
+//       let data: string = '';
+//       res.on('data', (chunk: Buffer) => {
+//         data += chunk.toString();
+//       });
+//       res.on('end', () => {
+//         resolve(data);
+//       });
+//     });
 
-    req.on('error', (error: Error) => {
-      console.error(error);
-      reject(error);
-    });
+//     req.on('error', (error: Error) => {
+//       console.error(error);
+//       reject(error);
+//     });
 
-    req.end();
-  });
-};
+//     req.end();
+//   });
+// };
+
+const getInstanceId = async () => {
+  const client = new EC2Client({ region: "ap-northeast-2" });
+  const input = {
+    DryRun: false
+  }
+  const command = new GetInstanceMetadataDefaultsCommand(input);
+  try {
+    const response = await client.send(command);
+    console.log(response)
+  }
+  catch (e) {
+    console.error(e)
+  }
+
+}
 
 const startMetricsExporter = async () => {
   // const instanceId = await getInstanceId()
-  const metadata = new EC2MetadataCredentials();
-  const instanceId = metadata.instanceId;
-  const options = {
-    url:
-      'http://' + "localhost:4318" + '/v1/metrics', // Grafana Agent Metric을 받는 url
-    headers: {},
-    concurrencyLimit: 5
-  }
-  const exporter = new OTLPMetricExporter(options)
+  getInstanceId()
+  // // const instanceId = metadata.instanceId;
+  // const options = {
+  //   url:
+  //     'http://' + "localhost:4318" + '/v1/metrics', // Grafana Agent Metric을 받는 url
+  //   headers: {},
+  //   concurrencyLimit: 5
+  // }
+  // const exporter = new OTLPMetricExporter(options)
   
-  console.log('instanceId:', instanceId)
+  // console.log('instanceId:', instanceId)
 
-  const resource = new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: 'backend-admin-metric',
-    [SemanticResourceAttributes.SERVICE_VERSION]: '1.0.0',
-    [SemanticResourceAttributes.SERVICE_INSTANCE_ID]: instanceId,
-    environment: 'production' // 사용자 정의 속성
-  })
+  // const resource = new Resource({
+  //   [SemanticResourceAttributes.SERVICE_NAME]: 'backend-admin-metric',
+  //   [SemanticResourceAttributes.SERVICE_VERSION]: '1.0.0',
+  //   [SemanticResourceAttributes.SERVICE_INSTANCE_ID]: instanceId,
+  //   environment: 'production' // 사용자 정의 속성
+  // })
 
-  // Creates MeterProvider and installs the exporter as a MetricReader
-  const meterProvider = new MeterProvider({
-    resource: resource
-  })
-  meterProvider.addMetricReader(
-    new PeriodicExportingMetricReader({
-      exporter,
-      exportIntervalMillis: 1000
-    })
-  )
+  // // Creates MeterProvider and installs the exporter as a MetricReader
+  // const meterProvider = new MeterProvider({
+  //   resource: resource
+  // })
+  // meterProvider.addMetricReader(
+  //   new PeriodicExportingMetricReader({
+  //     exporter,
+  //     exportIntervalMillis: 1000
+  //   })
+  // )
 
-  const hostMetrics = new HostMetrics({
-    meterProvider,
-    name: 'backend-admin-host-metric'
-  })
-  hostMetrics.start()
+  // const hostMetrics = new HostMetrics({
+  //   meterProvider,
+  //   name: 'backend-admin-host-metric'
+  // })
+  // hostMetrics.start()
 }
 
 export default startMetricsExporter
