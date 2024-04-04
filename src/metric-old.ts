004be97d83f4eb2c79ecc3ae41383aa387c4a481
Kang-Eunsu <kanges802@g.skku.edu>
2024-04-04 13:00:43 +0900

    fix

--- src/metric-old.ts
+++ src/metric-old.ts
@@ -6,15 +6,52 @@ import {
   MeterProvider,
   PeriodicExportingMetricReader
 } from '@opentelemetry/sdk-metrics'
+import os from 'os'
 import * as http from 'http';
-import { EC2MetadataCredentials } from 'aws-sdk';
+import { get } from 'http'
+import * as dotenv from 'dotenv'
 
+dotenv.config()
 
-const getInstanceId = (): Promise<string> => {
+const getMetadataToken = (): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const options = {
+      method: 'PUT',
+      hostname: '169.254.169.254',
+      path: '/latest/api/token',
+      headers: {
+        'X-aws-ec2-metadata-token-ttl-seconds': '21600', // 토큰 유효 시간(초)
+      },
+      timeout: 1000, // 요청 타임아웃
+    };
+
+    const req = http.request(options, (res) => {
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        resolve(data); // 토큰 반환
+      });
+    });
+
+    req.on('error', (error) => {
+      console.error(error);
+      reject(error);
+    });
+
+    req.end();
+  });
+};
+
+const getInstanceId = (token: string): Promise<string> => {
   return new Promise((resolve, reject) => {
     const options = {
       hostname: '169.254.169.254',
       path: '/latest/meta-data/instance-id',
+      headers: {
+        'X-aws-ec2-metadata-token': token
+      },
       method: 'GET',
       timeout: 1000,
     };
@@ -39,9 +76,10 @@ const getInstanceId = (): Promise<string> => {
 };
 
 const startMetricsExporter = async () => {
-  // const instanceId = await getInstanceId()
-  const metadata = new EC2MetadataCredentials();
-  const instanceId = metadata.instanceId;
+  const token = await getMetadataToken()
+  // const token = process.env.TOKEN
+  console.log('token:', token)
+  const instanceId = await getInstanceId(token)
   const options = {
     url:
       'http://' + "localhost" + '/v1/metrics', // Grafana Agent Metric을 받는 url
